2f7489d9043f5d7166e852ebb29d0f8006d35b2b
Michal Nedzka <michael.nedzka@gmail.com>
2019-01-10 23:23:07 +0100

    Create header and fix babel runtime version

---
+++ src/UI/icons.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import mcitylogo from "../../Resources/images/logos/manchester_city_logo.png";
+
+export const CityLogo = () => {
+  const template = <div>logo</div>;
+
+  if (props.link) {
+    return (
+      <Link to={props.linkTo} className="link_logo">
+        {template}
+      </Link>
+    );
+  } else {
+    return template;
+  }
+};
