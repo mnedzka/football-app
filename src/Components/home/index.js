5a24f1a8852ea1abf1b27016918aa9064aa6511a
Michal Nedzka <michael.nedzka@gmail.com>
2019-01-11 23:51:32 +0100

    Add stripes, Text & Featured components and start conecting them to routes

---
+++ src/Components/home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Featured from "./featured";
+
+const index = () => {
+  return (
+    <div className="bc_blue">
+      <Featured />
+    </div>
+  );
+};
+
+export default index;
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
 import React from "react";
 import Layout from "./HOC/layout";
+import { Switch, Route } from "react-router-dom";
+import Home from "./Components/home";
 
 const Routes = props => {
-  return <Layout>second line</Layout>;
+  return (
+    <Layout>
+      <Switch>
+        <Route>
+          <Route exact component={Home} path="/" />
+        </Route>
+      </Switch>
+    </Layout>
+  );
 };
 export default Routes;
