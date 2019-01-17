845a25b620a63d50950ce412a3777641893bfeb3
Michal Nedzka <michael.nedzka@gmail.com>
2019-01-17 23:22:42 +0100

    Finishing start page

--- src/Components/home/featured/Text.js
+++ src/Components/home/featured/Text.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { easePolyOut } from "d3-ease";
 import Animate from "react-move/Animate";
 
+import FeaturedPlayer from "../../../Resources/images/featured_player.png";
+
 class Text extends Component {
   animatedNumber = () => (
     <Animate
@@ -63,11 +65,70 @@ class Text extends Component {
     </Animate>
   );
 
+  animatedSecond = () => (
+    <Animate
+      show={true}
+      start={{
+        opacity: 0,
+        x: 503,
+        y: 586
+      }}
+      enter={{
+        opacity: [1],
+        x: [273],
+        y: [586],
+        timing: { delay: 300, duration: 1000, ease: easePolyOut }
+      }}
+    >
+      {({ opacity, x, y }) => {
+        return (
+          <div
+            className="featured_first"
+            style={{
+              opacity,
+              transform: `translate(${x}px, ${y}px)`
+            }}
+          >
+            Championships
+          </div>
+        );
+      }}
+    </Animate>
+  );
+
+  animatedPlayer = () => (
+    <Animate
+      show={true}
+      start={{
+        opacity: 0
+      }}
+      enter={{
+        opacity: [1],
+        timing: { delay: 800, duration: 1000, ease: easePolyOut }
+      }}
+    >
+      {({ opacity }) => {
+        return (
+          <div
+            className="featured_player"
+            style={{
+              opacity,
+              background: `url(${FeaturedPlayer})`,
+              transform: `translate(550px, 201px)`
+            }}
+          />
+        );
+      }}
+    </Animate>
+  );
+
   render() {
     return (
       <div className="featured_text">
+        {this.animatedPlayer()}
         {this.animatedNumber()}
         {this.animatedFirst()}
+        {this.animatedSecond()}
       </div>
     );
   }
