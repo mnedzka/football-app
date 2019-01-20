22182259e9aef5aa92646db01222850712e2ea33
Michal Nedzka <michael.nedzka@gmail.com>
2019-01-20 22:33:50 +0100

    Add matches component

---
+++ src/Components/UI/misc.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const Tag = ({ bck, size, color, children, link, linkTo }) => {
+  const template = (
+    <div
+      style={{
+        background: bck,
+        fontSize: size,
+        color: color,
+        padding: "5px 10px",
+        display: "inline-block",
+        fontFamily: "Righteous"
+      }}
+    >
+      {children}
+    </div>
+  );
+
+  if (link) {
+    return <Link to={linkTo}>{template}</Link>;
+  } else {
+    return template;
+  }
+};
