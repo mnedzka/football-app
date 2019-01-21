84d133fd4afc7525cc07f39e5b87e5a233f24dde
Michal Nedzka <michael.nedzka@gmail.com>
2019-01-21 22:23:57 +0100

    Add firebase connection

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/app";
+import "firebase/database";
+
+const config = {
+  apiKey: "AIzaSyDc8voz9m_fgZIPB4ZkFwgTRTm_gRbj7pQ",
+  authDomain: "man-city-374c7.firebaseapp.com",
+  databaseURL: "https://man-city-374c7.firebaseio.com",
+  projectId: "man-city-374c7",
+  storageBucket: "man-city-374c7.appspot.com",
+  messagingSenderId: "186516693877"
+};
+
+firebase.initializeApp(config);
+
+const firebaseDB = firebase.database();
+
+firebaseDB
+  .ref("matches")
+  .once("value")
+  .then(snapshot => {
+    console.log(snapshot.val());
+  });
