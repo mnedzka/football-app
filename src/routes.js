c282c0c1c1a7a676fc71f723c35b466c5d5181f6
Michal Nedzka <michael.nedzka@gmail.com>
2019-02-03 21:52:10 +0100

    Add PrivateRoutes component with authentication from firebase

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,21 @@ import Layout from "./HOC/layout";
 import { Switch, Route } from "react-router-dom";
 import Home from "./Components/home";
 import SignIn from "./Components/SignIn";
-
 import Dashboard from "./Components/Admin/Dashboard";
+import PrivateRoute from "./Components/AuthRoutes/PrivateRoutes";
 
 const Routes = props => {
+  console.log(props.user);
   return (
     <Layout>
       <Switch>
-        <Route exact component={Dashboard} path="/dashboard" />
+        <PrivateRoute
+          {...props}
+          path="/dashboard"
+          exact
+          component={Dashboard}
+        />
+        {/* <Route exact component={Dashboard} path="/dashboard" /> */}
         <Route exact component={SignIn} path="/sign_in" />
         <Route exact component={Home} path="/" />
       </Switch>
