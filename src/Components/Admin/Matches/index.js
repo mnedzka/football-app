a0384ef7effbc2c97e6f0a8e7ddbaed419741c8f
Michal Nedzka <michael.nedzka@gmail.com>
2019-02-04 21:42:49 +0100

    Add table from MaterialUI with loop for displaying data

--- src/Components/Admin/Matches/index.js
+++ src/Components/Admin/Matches/index.js
@@ -9,7 +9,7 @@ import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import CircularProgress from "@material-ui/core/CircularProgress";
 
-import { firebaseMatches } from "../../../firebase";
+import { firebaseMatches, firebase } from "../../../firebase";
 import { firebaseLooper } from "../../UI/misc";
 
 class AdminMatches extends Component {
@@ -18,15 +18,67 @@ class AdminMatches extends Component {
     matches: []
   };
 
+  componentDidMount() {
+    firebaseMatches.once("value").then(snapshot => {
+      const matches = firebaseLooper(snapshot);
+
+      this.setState({
+        isLoading: false,
+        matches: matches.reverse()
+      });
+    });
+  }
+
   render() {
     return (
       <AdminLayout>
-        <div className="admin_progress">
-          {this.state.isLoading ? (
-            <CircularProgress thickness={7} style={{ color: "98c5e9" }} />
-          ) : (
-            ""
-          )}
+        <div>
+          <Paper>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Date</TableCell>
+                  <TableCell>Match</TableCell>
+                  <TableCell>Result</TableCell>
+                  <TableCell>Final</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {this.state.matches
+                  ? this.state.matches.map((match, i) => (
+                      <TableRow key={i}>
+                        <TableCell>{match.date}</TableCell>
+                        <TableCell>
+                          <Link to={`/admin_matches/edit_match/${match.id}`}>
+                            {match.away} <strong>-</strong> {match.local}
+                          </Link>
+                        </TableCell>
+                        <TableCell>
+                          {match.resultAway} <strong>-</strong>{" "}
+                          {match.resultLocal}
+                        </TableCell>
+                        <TableCell>
+                          {match.final === "Yes" ? (
+                            <span className="matches_tag_red">Final</span>
+                          ) : (
+                            <span className="matches_tag_green">
+                              Not played yet
+                            </span>
+                          )}
+                        </TableCell>
+                      </TableRow>
+                    ))
+                  : null}
+              </TableBody>
+            </Table>
+          </Paper>
+          <div className="admin_progress">
+            {this.state.isLoading ? (
+              <CircularProgress thickness={7} style={{ color: "98c5e9" }} />
+            ) : (
+              ""
+            )}
+          </div>
         </div>
       </AdminLayout>
     );
