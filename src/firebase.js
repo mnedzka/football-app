9e0bf5f2dfa6e0b5256a0ac5aea7849f1abb6fba
Michal Nedzka <michael.nedzka@gmail.com>
2019-02-10 20:11:08 +0100

    Start to implement FileUploader component

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import firebase from "firebase/app";
 import "firebase/app";
 import "firebase/database";
 import "firebase/auth";
+import "firebase/storage";
 
 const config = {
   apiKey: "AIzaSyDc8voz9m_fgZIPB4ZkFwgTRTm_gRbj7pQ",
